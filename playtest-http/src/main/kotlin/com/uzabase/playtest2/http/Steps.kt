7cb6cad3c8abaf57856724ae2796128bec52f871
ayato-p <ayato-p@panchromatic.org>
2023-11-27 08:40:44 +0900

    First step of Playtest2!

--- playtest-http/src/main/kotlin/com/uzabase/playtest2/http/Steps.kt
+++ playtest-http/src/main/kotlin/com/uzabase/playtest2/http/Steps.kt
@@ -1,5 +1,30 @@
 package com.uzabase.playtest2.http
 
+import com.thoughtworks.gauge.Step
+import com.thoughtworks.gauge.datastore.ScenarioDataStore
+import okhttp3.Request
+
 class Steps {
 
+    private val client = okhttp3.OkHttpClient().newBuilder().build()
+
+    @Step("パス<path>に")
+    fun pathIntoRequest(path: String) {
+        ScenarioDataStore.put(KEY.REQUEST_PATH, path)
+    }
+
+    @Step("GETリクエストを送る")
+    fun sendGetRequest() {
+        val path = ScenarioDataStore.get(KEY.REQUEST_PATH) as String
+        val request = Request.Builder()
+            .url("http://localhost:8080$path")
+            .build()
+        val response = client.newCall(request).execute()
+        ScenarioDataStore.put(KEY.RESPONSE, response)
+    }
+
+    enum class KEY() {
+        REQUEST_PATH,
+        RESPONSE
+    }
 }
--- playtest-http/src/test/kotlin/com/uzabase/playtest2/http/StepsTest.kt
+++ playtest-http/src/test/kotlin/com/uzabase/playtest2/http/StepsTest.kt
@@ -1,10 +1,31 @@
 package com.uzabase.playtest2.http
 
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.thoughtworks.gauge.datastore.ScenarioDataStore
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 class StepsTest : FunSpec({
-    test("First test") {
-        1 shouldBe 1
+    val sut = Steps()
+    val server = WireMockServer(8080).also { it.start() }
+
+    beforeTest {
+        // Simulate Gauge's ScenarioDataStore being cleared between scenarios
+        ScenarioDataStore.items().forEach { k ->
+            ScenarioDataStore.remove(k)
+        }
+
+        server.start()
+    }
+
+    afterTest {
+        server.stop()
+    }
+
+    test("Hello, world") {
+        sut.pathIntoRequest("/hello")
+        sut.sendGetRequest()
+        val response = ScenarioDataStore.get(Steps.KEY.RESPONSE) as okhttp3.Response
+        response.body.string().shouldBe("Hello, world!")
     }
 })
