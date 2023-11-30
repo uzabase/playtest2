a0b2dcce204dc035eca4e16f9872f194a2fe1625
ayato-p <ayato-p@panchromatic.org>
2023-11-30 09:12:51 +0900

    Fix example project

--- examples/simple-api-test/src/test/kotlin/Steps.kt
+++ examples/simple-api-test/src/test/kotlin/Steps.kt
@@ -2,13 +2,21 @@ import com.example.demoapp.App
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.thoughtworks.gauge.AfterSuite
 import com.thoughtworks.gauge.BeforeSuite
-import com.thoughtworks.gauge.Step
+import com.uzabase.playtest2.Configuration
+import com.uzabase.playtest2.config.http
+import com.uzabase.playtest2.config.wireMock
+import com.uzabase.playtest2.plus
+import java.net.URI
 
 class Steps {
     private val innerApi = WireMockServer(3000)
 
     @BeforeSuite
     fun beforeSuite() {
+        Configuration.playtest2 {
+            http(URI("http://localhost:8080").toURL()) +
+                    wireMock("InnerAPI", URI("http://localhost:3000").toURL())
+        }
         App.startServer()
         innerApi.start()
     }
