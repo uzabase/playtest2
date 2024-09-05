6f1998c441f834b779bc7130b45ca173606122e8
ayato-p <ayato-p@panchromatic.org>
2024-09-05 23:21:20 +0900

    tweak

--- playtest-http/src/main/kotlin/com/uzabase/playtest2/http/ZoomJsonSteps.kt
+++ playtest-http/src/main/kotlin/com/uzabase/playtest2/http/ZoomJsonSteps.kt
@@ -5,15 +5,14 @@ import com.thoughtworks.gauge.Step
 import com.thoughtworks.gauge.datastore.ScenarioDataStore
 import com.uzabase.playtest2.core.K
 import com.uzabase.playtest2.core.ScenarioDataStoreExt.ensureGet
-import com.uzabase.playtest2.core.proxy.ProxyFactory
 import com.uzabase.playtest2.core.proxy.AssertableProxyFunctions
-import okhttp3.ResponseBody
+import com.uzabase.playtest2.core.proxy.ProxyFactory
 
 class ZoomJsonSteps {
     @Step("JSONのパス<jsonPath>に対応する値が")
     fun zoomJson(jsonPath: String): Any =
-        ensureGet<ResponseBody>(K.AssertionTarget)
-            .let { JsonPath.read<Any>(it.byteStream(), jsonPath) }
+        ensureGet<String>(K.AssertionTarget)
+            .let { JsonPath.read<Any>(it, jsonPath) }
             .let { value ->
                 ProxyFactory.make<Any, Unit>(value, AssertableProxyFunctions(
                     asString = { value as String },
