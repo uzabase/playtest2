ac51c480a67718c5eb26daecba836faa61c1b20a
ayato-p <ayato-p@panchromatic.org>
2024-09-07 14:11:35 +0900

    Refine Proxy

--- playtest-http/src/main/kotlin/com/uzabase/playtest2/http/proxy/ResponseHeaders.kt
+++ playtest-http/src/main/kotlin/com/uzabase/playtest2/http/proxy/ResponseHeaders.kt
@@ -1,23 +1,24 @@
 package com.uzabase.playtest2.http.proxy
 
-import com.uzabase.playtest2.core.proxy.AssertableProxyFunctions
-import com.uzabase.playtest2.core.proxy.ProxyFactory
+import com.uzabase.playtest2.core.assertion.Assertable
 import com.uzabase.playtest2.core.zoom.Zoomable
 import java.net.http.HttpHeaders
 
-
-fun HttpHeaders.toProxy(): Any =
-    this.let { headers: HttpHeaders ->
-        ProxyFactory.make<HttpHeaders, String>(
-            headers,
-            AssertableProxyFunctions(
-                asString = {
-                    headers.map().entries.sortedBy { (k, _) -> k }
-                        .joinToString(System.lineSeparator()) { (k, v) -> "$k: ${v.joinToString(",")}" }
-                }
-            ),
-            zoomable = object : Zoomable<String> {
-                override fun zoom(key: String): Any? = headers.map()[key]?.joinToString(",")
-            }
-        )
+class HttpHeadersProxy private constructor(val headers: HttpHeaders) : Assertable, Zoomable<String> {
+    companion object {
+        fun of(headers: HttpHeaders): HttpHeadersProxy {
+            return HttpHeadersProxy(headers)
+        }
     }
+
+    override fun asString(): String =
+        headers.map().entries.sortedBy { (k, _) -> k }
+            .joinToString(System.lineSeparator()) { (k, v) -> "$k: ${v.joinToString(",")}" }
+
+    override fun asRaw(): HttpHeaders = headers
+
+    override fun asLong(): Long = throw UnsupportedOperationException("Cannot convert to Long")
+    override fun asBoolean(): Boolean = throw UnsupportedOperationException("Cannot convert to Boolean")
+
+    override fun zoom(key: String): Any? = headers.map()[key]?.joinToString(",")
+}
