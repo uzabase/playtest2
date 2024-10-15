6f4a68ceea7567cf05e1d851d1f0f102a94a6507
ayato-p <ayato-p@panchromatic.org>
2024-10-16 08:06:07 +0900

    refactor for json path proxy

--- playtest-http/src/main/kotlin/com/uzabase/playtest2/http/zoom/JsonSerializable.kt
+++ playtest-http/src/main/kotlin/com/uzabase/playtest2/http/zoom/JsonSerializable.kt
@@ -9,21 +9,41 @@ fun interface JsonSerializable {
     fun toJsonPathProxy(path: String): JsonPathProxy
 }
 
-class JsonPathProxy internal constructor(
-    val json: String,
-    val path: String
-) : ShouldBeString, ShouldContainsString, ShouldMatchString, ShouldBeLong, ShouldBeBigDecimal, ShouldBeBoolean, ShouldBeExist, ShouldNotBeExist {
-    override fun shouldBe(expected: String): Boolean = JsonPath.read<String>(json, path) == expected
-    override fun shouldContain(expected: String): Boolean = JsonPath.read<String>(json, path).contains(expected)
-    override fun shouldMatch(expected: String): Boolean =
-        expected.toRegex().matches(JsonPath.read<String>(json, path))
+sealed interface JsonPathProxy : ShouldBeString, ShouldContainsString, ShouldMatchString, ShouldBeLong,
+    ShouldBeBigDecimal, ShouldBeBoolean, ShouldBeExist, ShouldNotBeExist {
+    companion object {
+        fun of(json: String, path: String): JsonPathProxy =
+            JsonPath.compile(path).let { compiled ->
+                if(compiled.isDefinite) {
+                    DefiniteJsonPathProxy.of(json, compiled)
+                } else {
+                    TODO()
+                }
+            }
+    }
+}
 
-    override fun shouldBe(expected: Long): Boolean = JsonPath.read<Int>(json, path).toLong() == expected
-    override fun shouldBe(expected: BigDecimal): Boolean = JsonPath.read<Double>(json, path).toBigDecimal() == expected
-    override fun shouldBe(expected: Boolean): Boolean = JsonPath.read<Boolean>(json, path) == expected
+internal class DefiniteJsonPathProxy private constructor(
+    private val json: String,
+    private val path: JsonPath
+) : JsonPathProxy {
+    companion object {
+        fun of(json: String, path: JsonPath): JsonPathProxy = DefiniteJsonPathProxy(json, path)
+    }
+
+    override fun shouldBe(expected: String): Boolean = JsonPath.parse(json).read<String>(path) == expected
+    override fun shouldContain(expected: String): Boolean = JsonPath.parse(json).read<String>(path).contains(expected)
+    override fun shouldMatch(expected: String): Boolean =
+        expected.toRegex().matches(JsonPath.parse(json).read<String>(path))
+
+    override fun shouldBe(expected: Long): Boolean = JsonPath.parse(json).read<Int>(path).toLong() == expected
+    override fun shouldBe(expected: BigDecimal): Boolean =
+        JsonPath.parse(json).read<Double>(path).toBigDecimal() == expected
+
+    override fun shouldBe(expected: Boolean): Boolean = JsonPath.parse(json).read<Boolean>(path) == expected
     override fun shouldBeExist(): Boolean =
         try {
-            JsonPath.read<Any>(json, path)
+            JsonPath.parse(json).read<Any>(path)
             true
         } catch (e: PathNotFoundException) {
             false
