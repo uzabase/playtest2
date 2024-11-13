90ddd3294181008f67077c6d0b10814612d661b6
ayato-p <ayato-p@panchromatic.org>
2024-11-13 19:15:15 +0900

    tweak

--- examples/database/src/test/kotlin/Sample.kt
+++ examples/database/src/test/kotlin/Sample.kt
@@ -1,15 +1,13 @@
 import com.thoughtworks.gauge.AfterSuite
 import com.thoughtworks.gauge.BeforeSuite
-import com.thoughtworks.gauge.Step
 import com.uzabase.playtest2.core.config.Configuration
 import com.uzabase.playtest2.jdbc.config.jdbc
 import org.testcontainers.containers.PostgreSQLContainer
-import java.sql.DriverManager
 import java.util.logging.Level
 import java.util.logging.LogManager
 
 class Sample {
-    val container = PostgreSQLContainer<Nothing>("postgres:17.0-bookworm").apply {
+    private val container = PostgreSQLContainer<Nothing>("postgres:17.0-bookworm").apply {
         withDatabaseName("testdatabase")
         withUsername("michel")
         withPassword("goodpassphrase")
@@ -31,15 +29,5 @@ class Sample {
     @AfterSuite
     fun afterSuite() {
         container.stop()
-    }
-
-    @Step("Hello world")
-    fun sampleStep() {
-        DriverManager.getConnection(container.jdbcUrl, container.username, container.password).use { conn ->
-            val rs = conn.prepareStatement("select id, name from users").executeQuery()
-            while (rs.next()) {
-                println("${rs.getInt(1)}: ${rs.getString(2)}")
-            }
-        }
     }
 }
